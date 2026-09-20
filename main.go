8e0454552ccf15a47f3edaea05ac798e42cbb7b9
Dan Collins <dan.collins.dev@gmail.com>
2026-09-20 14:19:39 -0400

    stubbed out basic app loop

--- main.go
+++ main.go
@@ -1,12 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	todaysTemp := toCelsius(91.4)
-	tomorrowsTemp := toFahrenheit(0)
-
-	fmt.Println(todaysTemp, tomorrowsTemp)
+	printTitle()
+	printMainOptions()
+	appLoop()
 }
 
 func toCelsius(fTemp float64) float64 {
@@ -15,4 +16,45 @@ func toCelsius(fTemp float64) float64 {
 
 func toFahrenheit(cTemp float64) float64 {
 	return (cTemp * (9.0 / 5.0)) + 32
+}
+
+func printTitle() {
+	fmt.Println("===============================")
+	fmt.Println("| Temperature Converter in Go |")
+	fmt.Println("===============================")
+}
+
+func printMainOptions() {
+	fmt.Println("[1] Convert temperature from Fahrenheit to Celsius")
+	fmt.Println("[2] Convert temperature from Celsius to Fahrenheit")
+	fmt.Println("[3] Quit Program")
+}
+
+func appLoop() {
+	for {
+		var choice string
+
+		fmt.Print("\nSelect an option: ")
+		_, err := fmt.Scan(&choice)
+
+		if err != nil {
+			fmt.Println("Error reading input:", err)
+			return
+		}
+
+		switch choice {
+		case "1":
+			fmt.Println("Fahrenheit Celsius")
+
+		case "2":
+			fmt.Println("Celsius to Fahrenheit")
+
+		case "3":
+			fmt.Println("Quitting application")
+			return
+
+		default:
+			fmt.Println("Invalid option")
+		}
+	}
 }
