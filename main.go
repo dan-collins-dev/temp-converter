22776d75d74a0b6f0559ab9ce18a08dfb87c23d1
Dan Collins <dan.collins.dev@gmail.com>
2026-09-20 13:01:48 -0400

    core functions added

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
+import "fmt"
+
 func main() {
-	println("Initial commit for temperature converter program")
+	todaysTemp := toCelsius(91.4)
+	tomorrowsTemp := toFahrenheit(0)
+
+	fmt.Println(todaysTemp, tomorrowsTemp)
+}
+
+func toCelsius(fTemp float64) float64 {
+	return (fTemp - 32) * (5.0 / 9.0)
+}
+
+func toFahrenheit(cTemp float64) float64 {
+	return (cTemp * (9.0 / 5.0)) + 32
 }
